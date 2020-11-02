d8c277466b66b123a75cab083b7ed77e736de13f
blackplusy <osimeno@163.com>
2020-11-02 14:41:17 +0800

    Add files via upload

---
+++ 例子-1102-06.成员运算符.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+#字符串
+name='oh my god!'
+for i in name:
+    print(i)
+
+#列表
+l=['kobe','jordan','rose']
+for i in l:
+    print(i)
+
+#判断
+if 'e' in name:
+    print('e is here!')
+else:
+    print('not here!')
