1b546aa6d52ad808d2fcea6af18bf0f9761b8cad
blackplusy <osimeno@163.com>
2020-11-02 10:43:26 +0800

    Add files via upload

---
+++ 例子-1102-02.python的输入.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+#输入是把input函数的值传入变量中，操作变量
+#input() 括号中填写提示信息
+#name=input()
+#print(name)
+
+name=input('请输入你的法号：')
+print('您的法号是%s' % name)
+print(type(name))
+
+
+num1=int(input('请输入您的年龄:'))
+print(type(num1))
