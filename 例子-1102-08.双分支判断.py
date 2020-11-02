b32ac5cf15ceaea998522a7f525cc6aca1a97035
blackplusy <osimeno@163.com>
2020-11-02 16:15:46 +0800

    Add files via upload

---
+++ 例子-1102-08.双分支判断.py
@@ -0,0 +1,8 @@
+#coding=utf-8
+
+name=input('为什么叫我贝塔？')
+
+if name=='巧碧螺':
+    print('yes its me!')
+else:
+    print('飞机刷没了!!')
---
+++ 例子-1102-09.多分支判断.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+
+name=input('请输入您喜欢的主播')
+
+if name=='黑哥GaGa':
+    print('来！刷火箭！')
+elif name=='55开':
+    print('难受啊，马飞！')
+elif name=='大司马':
+    print('正方形打野！')
+else:
+    print('来，老铁们，刷波666，火箭走起！爱你们哟！')
+    
